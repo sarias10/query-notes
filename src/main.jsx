c476de9fc73da1e70cce9e96928d53e6168d7caa
Sergio Arias <s-andres140@hotmail.com>
2025-04-14 13:42:09 -0500

    feat: :recycle: app works

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={QueryClient}>
+  <QueryClientProvider client={queryClient}>
     <App />,
   </QueryClientProvider>
 
