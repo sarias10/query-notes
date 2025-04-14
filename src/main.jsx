09fc3d68ef658b44628348817b1860fa5db1b88b
Sergio Arias <s-andres140@hotmail.com>
2025-04-14 13:27:15 -0500

    Add @tanstack/react-query for data fetching and wrap App component in QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />,
+  <QueryClientProvider client={QueryClient}>
+    <App />,
+  </QueryClientProvider>
+
 )
